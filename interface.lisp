bdb4be4a2019b9467b28c57668b821b7203913f5
Thayne McCombs <astrothayne@gmail.com>
2015-08-30 17:20:20 -0600

    Add grow-from-iterable to colliflower.

--- interface.lisp
+++ interface.lisp
@@ -7,4 +7,13 @@
   (:documentation "Combination of the LITER, SILO, and GARTEN packages.")
   (:use-reexport :liter
                  :silo
-                 :garten))
+                 :garten)
+  (:export #:grow-from-iterable))
+
+(defun grow-from-iterable (iterable type &rest type-args)
+  "Create a growable of type TYPE from an iterable.
+
+TYPE-ARGS are additional arguments to pass to MAKE-GROWER after the type."
+  (let ((grower (apply #'make-grower type type-args)))
+    (feed-iterable grower iterable)
+    (fruit grower)))
