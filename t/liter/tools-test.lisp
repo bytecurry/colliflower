5d9e9b8e5e9191613b397293b434d235be56fe50
Thayne McCombs <astrothayne@gmail.com>
2015-09-17 22:18:20 -0600

    Add more tests for liter/tools.

--- t/liter/tools-test.lisp
+++ t/liter/tools-test.lisp
@@ -6,7 +6,7 @@
 
 (in-package liter/tools-test)
 
-(plan nil)
+(plan 10)
 
 (subtest "itransform"
   (let ((it (itransform (icounter)
@@ -97,5 +97,66 @@
     (is (inext it) '(1 b))
     (is (inext it) '(2 a))
     (is (inext it) '(3 b))))
+
+(subtest "itee"
+  (let ((c (icounter)))
+    (multiple-value-bind (i1 i2) (itee c)
+      (is (inext i1) 0)
+      (is (inext i1) 1)
+      (is (inext i1) 2)
+      (is (inext i2) 0)
+      (is (inext i2) 1)
+      (is (inext i2) 2)
+      (is (inext i2) 3)
+      (is (inext i1) 3)))
+  (multiple-value-bind (one two three) (itee '(:foo :bar :car) 3)
+    (is (iterator-list one) '(:foo :bar :car))
+    (is-error (funcall one) 'iteration-ended)
+    (is (funcall two) :foo)
+    (is (funcall three) :foo)
+    (is (iterator-list two) '(:bar :car))
+    (is (iterator-list three) '(:bar :car))))
+
+(subtest "take and drop"
+  (subtest "itake"
+    (is (iterator-list (itake (icounter) 5))
+        '(0 1 2 3 4))
+    (is (iterator-list (itake '(1 2 3) 5))
+        '(1 2 3))
+    (is (iterator-list (itake '(1 2 3) 1))
+        '(1))
+    (is (iterator-list (itake (irepeat :foo) 0))
+        nil))
+  (subtest "itake-while"
+    (is (iterator-list (itake-while (icounter :from 1) (lambda (n)
+                                                   (< n 6))))
+        '(1 2 3 4 5))
+    (is (iterator-list (itake-while #(1 2 3) (constantly t)))
+        '(1 2 3))
+    (is (iterator-list (itake-while #(1 2 3) (constantly nil)))
+        nil)
+    (is (iterator-list (itake-while #(1 3 6 9 2 4 8) #'oddp))
+        '(1 3)))
+  (subtest "idrop"
+    (is (iterator-list (idrop (icounter :to 10) 6))
+        '(6 7 8 9))
+    (is (iterator-list (idrop #(1 2 3) 0))
+        '(1 2 3))
+    (is (iterator-list (idrop #(1 2) 10))
+        nil)
+    (let ((it (idrop (icycle* 1 2 3) 5)))
+      (is (inext it) 3)
+      (is (inext it) 1)))
+  (subtest "idrop-while"
+    (is (iterator-list (idrop-while #(1 3 7 8 2 1 9) #'oddp))
+        '(8 2 1 9))
+    (is (iterator-list (idrop-while #(1 2 4 8) #'evenp))
+        '(1 2 4 8))
+    (is (iterator-list (idrop-while '(:foo :bar) (constantly t)))
+        nil)
+    (let ((it (idrop-while (icounter :from 1) (lambda (n)
+                                                (< n 6)))))
+      (is (inext it) 6)
+      (is (inext it) 7))))
 
 (finalize)
